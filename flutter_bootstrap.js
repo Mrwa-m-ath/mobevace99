ececd5b99f85458a3684d6a6446a8e405a9c2304
Mrwa-m-ath <79510267+Mrwa-m-ath@users.noreply.github.com>
2024-08-13 11:18:08 +0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3370631089"
+    serviceWorkerVersion: "3462606709"
   }
 });
