a92d20d0915143dbffc1200e47beda17f8243f0c
Mrwa-m-ath <79510267+Mrwa-m-ath@users.noreply.github.com>
2024-09-22 15:37:18 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8bba36e1cf3ffb432a8536e206101904
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "282219399ba82e126a9faf833c8feb00",
+"flutter_bootstrap.js": "f7adbf790648952195729ff68281481c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
